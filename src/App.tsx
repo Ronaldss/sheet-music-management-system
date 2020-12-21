aeaa4597b9c544ee9b6d96c702d1641fddda3ad6
Ronald Santos <ronaldcontact2019@gmail.com>
2020-12-21 17:45:33 -0300

    Begin

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TitleMusic from './components/TitleMusic/index'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <TitleMusic>Partituras</TitleMusic>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
---
+++ src/components/TitleMusic/index.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const TitleMusic = styled.h1`
+    color: #F00;
+    font-size: 32px;
+`;
+
+export default TitleMusic;

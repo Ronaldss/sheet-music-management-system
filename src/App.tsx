2ac630bc8c08a3f542887d77d0e5f39bc72b4d15
Emusc <44450158+Hilverton@users.noreply.github.com>
2020-12-22 19:32:45 -0300

    adding dependencies
    1. addition of eslint and prettier
    2. eslint and prettier configuration
    3. addition of firebase and react-dropzone

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import TitleMusic from './components/TitleMusic/index'
+import TitleMusic from './components/TitleMusic';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <TitleMusic>Partituras</TitleMusic>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TitleMusic>Partituras</TitleMusic>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/TitleMusic/index.ts
+++ src/components/TitleMusic/index.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 const TitleMusic = styled.h1`
-    color: #F00;
-    font-size: 32px;
+  color: #f00;
+  font-size: 32px;
 `;
 
 export default TitleMusic;

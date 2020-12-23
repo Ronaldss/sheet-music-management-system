fc5862a6cb471ef8d4ebbf1ff7d80e3851a91def
Emusc <44450158+Hilverton@users.noreply.github.com>
2020-12-22 21:21:09 -0300

    reversing in strength

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import TitleMusic from "./components/TitleMusic/index";
-=======
 import TitleMusic from './components/TitleMusic';
->>>>>>> 2ac630bc8c08a3f542887d77d0e5f39bc72b4d15
 
 const App: React.FC = () => {
   return (

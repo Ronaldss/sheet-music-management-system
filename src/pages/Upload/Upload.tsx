8c34fd32bbf1e81115c2b1702e6265aeea8e4cdf
Emusc <44450158+Hilverton@users.noreply.github.com>
2020-12-31 13:12:29 -0300

    Stylization of the Upload page

--- src/pages/Upload/Upload.tsx
+++ src/pages/Upload/Upload.tsx
@@ -1,11 +1,59 @@
-import React from "react";
-
-import { Container } from "./styles";
+import { useDropzone, FileWithPath } from "react-dropzone";
+import { ButtonMusic, InputMusic } from "../../components";
+import {
+  Container,
+  Title,
+  DropzoneContainer,
+  Section,
+  Subtitle,
+  List,
+  ListItem,
+} from "./styles";
 
 export default function Upload() {
+  const {
+    acceptedFiles,
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone();
+
+  const files = acceptedFiles.map((file: FileWithPath) => (
+    <ListItem key={file.path}>{file.path}</ListItem>
+  ));
+
+  console.log(files);
+
   return (
     <Container>
-      <h1>Upload</h1>
+      <Title>Upload de partituras</Title>
+      <InputMusic placeholder="Título da música" />
+      <DropzoneContainer
+        {...getRootProps({ isDragActive, isDragAccept, isDragReject })}
+      >
+        <input {...getInputProps()} />
+        <p>
+          Arraste e solte alguns arquivos aqui ou clique para selecionar
+          arquivos
+        </p>
+        <em>(apenas *.pdf serão aceitos)</em>
+      </DropzoneContainer>
+      {files.length > 0 && (
+        <section>
+          <Subtitle>Arquivos aceitos</Subtitle>
+          <List>{files}</List>
+        </section>
+      )}
+      <Section>
+        <div>
+          <ButtonMusic>Enviar</ButtonMusic>
+        </div>
+        <div>
+          <ButtonMusic>Cancelar</ButtonMusic>
+        </div>
+      </Section>
     </Container>
   );
 }
--- src/pages/Upload/styles.ts
+++ src/pages/Upload/styles.ts
@@ -1,5 +1,89 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { DropzoneRootProps } from "react-dropzone";
 
 export const Container = styled.div`
+  margin: 0 auto;
+  width: 80%;
+`;
 
+export const Title = styled.h1`
+  margin: 40px 0;
+  font-size: 2rem;
+  font-weight: bold;
+  text-align: center;
+`;
+
+const getColor = (props: DropzoneRootProps) => {
+  if (props.isDragAccept) {
+    return "#00e676";
+  }
+  if (props.isDragReject) {
+    return "#ff1744";
+  }
+  if (props.isDragActive) {
+    return "#2196f3";
+  }
+  return "#eeeeee";
+};
+
+export const DropzoneContainer = styled.div`
+  height: 200px;
+  padding: 20px;
+  margin: 10px 0 30px;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-width: 2px;
+  border-radius: 8px;
+  border-color: ${(props) => getColor(props)};
+  border-style: dashed;
+  background-color: #fafafa;
+  color: #bdbdbd;
+  outline: none;
+  transition: border 0.24s ease-in-out;
+`;
+
+export const Subtitle = styled.h4`
+  font-size: 1.3rem;
+  font-weight: bold;
+`;
+
+export const List = styled.ul`
+  margin: 20px 0 30px;
+  list-style: none;
+`;
+
+export const ListItem = styled.li`
+  background-color: aquamarine;
+  padding: 20px;
+  border: none;
+  border-radius: 8px;
+  margin-top: 10px;
+  margin-bottom: 0;
+`;
+
+export const Section = styled.section`
+  width: 50%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  div {
+    width: 100%;
+  }
+  div:first-child {
+    margin-right: 20px;
+
+    @media (max-width: 550px) {
+      margin-right: 0;
+      margin-bottom: 20px;
+    }
+  }
+
+  @media (max-width: 550px) {
+    width: 100%;
+    flex-direction: column;
+  }
 `;
